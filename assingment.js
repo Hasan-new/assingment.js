0a5de14bb00dfeb9ab785426f97cbbd0dab4a71c
Hasan-new <mdhasan.yahoo@gmail.com>
2020-07-17 16:49:20 +0600

    updated

---
+++ assingment.js
@@ -0,0 +1,29 @@
+// feetToMile
+function feetToMile(feet){
+    var mile = feet / 5280;
+    return mile;
+}
+var result = feetToMile(5280);
+console.log(result);
+// This is complete.
+
+
+// woodCalculator
+function woodCalculatro(chair,  table, bed){
+    var chairMeasure = chair * 1;
+    var tableMeasure = table * 3;
+    var bedMeasure = bed * 5;
+    var totalMeasure = chairMeasure + tableMeasure + bedMeasure;
+    return totalMeasure;
+}
+var result = woodCalculatro(1, 3, 5);
+console.log(result);
+
+
+// This is complete too.
+
+
+// brickCalculator
+function brickCalculator(params) {
+    
+}
